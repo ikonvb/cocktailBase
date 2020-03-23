5cbcddd7406585625048042c41ebf765cc5d1b2c
@kostyaFrom <korustlt@gmail.com>
2020-03-23 22:09:01 +0400

    init commit

---
+++ app/src/main/java/com/bulyginkonstantin/cocktailbase/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.bulyginkonstantin.cocktailbase.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bulyginkonstantin.cocktailbase.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
