b90e69fe5538ada45a9f9f28f00c1b19dfb23433
@kostyaFrom <korustlt@gmail.com>
2020-03-24 22:25:45 +0400

    implement adapter

---
+++ app/src/main/java/com/bulyginkonstantin/cocktailbase/adapters/CocktailListAdapter.kt
@@ -0,0 +1,33 @@
+package com.bulyginkonstantin.cocktailbase.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bulyginkonstantin.cocktailbase.R
+import com.bulyginkonstantin.cocktailbase.model.Cocktail
+import kotlinx.android.synthetic.main.detail_cocktail_info.view.*
+
+class CocktailListAdapter(private val cocktailList: ArrayList<Cocktail>) :
+    RecyclerView.Adapter<CocktailListAdapter.CocktailViewHolder>() {
+
+    fun updateCocktailList(newCocktailList: List<Cocktail>) {
+        cocktailList.clear()
+        cocktailList.addAll(newCocktailList)
+        notifyDataSetChanged()
+    }
+
+    class CocktailViewHolder(var view: View) : RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CocktailViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.detail_cocktail_info, parent, false)
+        return CocktailViewHolder(view)
+    }
+
+    override fun getItemCount() = cocktailList.size
+
+    override fun onBindViewHolder(holder: CocktailViewHolder, position: Int) {
+        holder.view.textViewCocktailName.text = cocktailList[position].strDrink
+    }
+}
