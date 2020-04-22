f4d4e1f700cc40f8bb2dcf746dfe382b0e5d8caa
@kostyaFrom <korustlt@gmail.com>
2020-04-22 19:03:17 +0400

    implement navigation drawer

--- app/src/main/java/com/bulyginkonstantin/cocktailbase/view/MainActivity.kt
+++ app/src/main/java/com/bulyginkonstantin/cocktailbase/view/MainActivity.kt
@@ -1,15 +1,43 @@
 package com.bulyginkonstantin.cocktailbase.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.bulyginkonstantin.cocktailbase.R
-import com.facebook.stetho.Stetho
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Stetho.initializeWithDefaults(this);
+        setSupportActionBar(toolbar)
+
+        navController = findNavController(R.id.nav_host_fragment)
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.mainFragment,
+                R.id.chooseSearchFragment,
+                R.id.searchByNameFragment
+            ), drawer_layout
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        nav_view.setupWithNavController(navController)
+
+        //Stetho.initializeWithDefaults(this)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
